0070173640ee5b0cca8886d9d98083ea11f9f95d
Piotr Makowski <pmakowski@notebooksbilliger.de>
2024-03-04 18:07:40 +0100

    Add logs for web logging in / registering

--- DynmapCore/src/main/java/org/dynmap/servlet/LoginServlet.java
+++ DynmapCore/src/main/java/org/dynmap/servlet/LoginServlet.java
@@ -3,6 +3,7 @@ package org.dynmap.servlet;
 import static org.dynmap.JSONUtils.s;
 
 import org.dynmap.DynmapCore;
+import org.dynmap.Log;
 import org.json.simple.JSONObject;
 
 import javax.servlet.*;
@@ -13,7 +14,11 @@ import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import org.dynmap.utils.IpAddressMatcher;
 
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -23,9 +28,20 @@ public class LoginServlet extends HttpServlet {
     public static final String LOGIN_PAGE = "../login.html";
     public static final String LOGIN_POST = "/up/login";
     private Charset cs_utf8 = Charset.forName("UTF-8");
-    
+    public ArrayList<IpAddressMatcher> proxyaddress = new ArrayList<IpAddressMatcher>();
+
     public LoginServlet(DynmapCore core) {
         this.core = core;
+        final List<String> trustedproxy = core.configuration.getStrings("trusted-proxies", null);
+        if(trustedproxy != null) {
+            for(String s : trustedproxy) {
+                this.proxyaddress.add(new IpAddressMatcher(s.trim()));
+            }
+        }
+        else {
+            this.proxyaddress.add(new IpAddressMatcher("127.0.0.1"));
+            this.proxyaddress.add(new IpAddressMatcher("0:0:0:0:0:0:0:1"));
+        }
     }
 
     @Override
@@ -45,6 +61,15 @@ public class LoginServlet extends HttpServlet {
         resp.setContentLength(b.length);
         resp.getOutputStream().write(b);
     }
+
+    private boolean trustedProxy(String ip) {
+    	for (IpAddressMatcher m : proxyaddress) {
+    		if (m.matches(ip)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -59,6 +84,18 @@ public class LoginServlet extends HttpServlet {
             sess.setMaxInactiveInterval(60);    /* Intialize to 60 seconds */
         }
         
+        /* If from trusted proxy, check for client */
+        String rmtaddr = req.getRemoteAddr();
+        if (this.trustedProxy(rmtaddr)) {	// If remote address is valid trusted proxy
+            /* If proxied client address, get original IP */
+            if (req.getHeader("X-Forwarded-For") != null) {
+                // Split list, since addresses after first are proxy chain
+                String[] proxyAddrs = req.getHeader("X-Forwarded-For").split(",");
+                // We only want first - any others are proxies that our local proxy was willing to pass to us
+                rmtaddr = proxyAddrs[0].trim();
+            }
+        }
+
         String uri = req.getRequestURI();
         if(uri.equals("/up/login")) {  /* Process login form */
             uid = req.getParameter("j_username");
@@ -66,10 +103,12 @@ public class LoginServlet extends HttpServlet {
             if((uid == null) || (uid.equals("")))
                 uid = USERID_GUEST;
             if(core.checkLogin(uid, pwd)) {
+                Log.info("User " + uid + " logged in from " + rmtaddr);
                 sess.setAttribute(USERID_ATTRIB, uid);
                 sendResponse(resp, "success");
             }
             else {
+                Log.warning("User " + uid + " failed to log in from " + rmtaddr);
                 sendResponse(resp, "loginfailed");
             }
         }
@@ -83,14 +122,17 @@ public class LoginServlet extends HttpServlet {
                 sendResponse(resp, "verifyfailed");
             }
             else if(core.registerLogin(uid, pwd, passcode)) {    /* Good registration? */
+                Log.info("User " + uid + " registered successfully from " + rmtaddr);
                 sess.setAttribute(USERID_ATTRIB, uid);
                 sendResponse(resp, "success");
             }
             else {
+                Log.warning("User " + uid + " failed to registered from " + rmtaddr);
                 sendResponse(resp, "registerfailed");
             }
         }
         else {
+            Log.severe("User failed to log in from " + rmtaddr);
             sendResponse(resp, "loginfailed");
         }
     }
